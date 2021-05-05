18086d1e7f3f07b7b843bc27074c7f28faf6b766
Ryan C-Love <77506420+ryanclove@users.noreply.github.com>
2021-05-04 21:28:28 -0400

    Create strbuf.h

---
+++ Assignments/Assignment 2/strbuf.h
@@ -0,0 +1,16 @@
+typedef struct {
+    size_t length;
+    size_t used;
+    char *data;
+    int index;
+    char item;
+    char *str;
+    char *sb;
+} strbuf_t;
+
+int sb_init(strbuf_t *, size_t);
+void sb_destroy(strbuf_t *);
+int sb_append(strbuf_t *, char);
+int sb_remove(strbuf_t *, char *);
+int sb_insert(strbuf_t *, int, char);
+int sb_concat(strbuf_t *, char);
